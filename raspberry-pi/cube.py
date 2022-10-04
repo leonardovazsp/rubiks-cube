4bfc4c823baa6b165ad6434073634df13985dee6
Leonardo <leonardovazsp@gmail.com>
2022-10-04 19:36:54 +1000

    cube.py move functions added - not tested yet

--- raspberry-pi/cube.py
+++ raspberry-pi/cube.py
@@ -1,9 +1,6 @@
 '''
 This module provides an API to interact with the Rubik's Cube.
 It also provides a virtual cube to keep track of the cube state.
-The cube state is represented as a tensor of shape (54, 6).
-The state is one-hot encoded, where each element is a vector of length 6,
-which represents the color of the cubelet.
 The state is stored in a file called `state.npy`.
 '''
 
@@ -26,7 +23,7 @@ class Cube:
             state = np.load('state.npy')
         else:
             state = np.arange(54) // 9 # 54 cubelets, 9 cubelets per face
-            state = np.eye(6)[state] # one-hot encode
+            state = state.reshape(6, 3, 3) # reshape to 6 faces, 3x3 cubelets
             np.save('state.npy', state) # save the state
         return state
 
@@ -47,11 +44,158 @@ class Cube:
                 move = move + '_rev'
             self.moves[self.moves_list.index(move)]()
         self.state = np.arange(54) // 9
-        self.state = np.eye(6)[self.state]
+        self.state = state.reshape(6, 3, 3)
         np.save('state.npy', self.state)
         self.history = np.array([])
         np.save('history.npy', self.history)
     
-    
+    def top(self):
+        # Rotate the top face clockwise
+        cd.top()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, 0] = temp_state[1, 0]
+        self.state[1, 0] = temp_state[2, 0]
+        self.state[2, 0] = temp_state[3, 0]
+        self.state[3, 0] = temp_state[0, 0]
+        self.state[4, :] = np.rot90(temp_state[4, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
 
+    def top_rev(self):
+        # Rotate the top face counter-clockwise
+        cd.top_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, 0] = temp_state[3, 0]
+        self.state[1, 0] = temp_state[0, 0]
+        self.state[2, 0] = temp_state[1, 0]
+        self.state[3, 0] = temp_state[2, 0]
+        self.state[4, :] = np.rot90(temp_state[4, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def right(self):
+        # Rotate the right face clockwise
+        cd.right()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, :, 2] = temp_state[5, :, 2]
+        self.state[4, :, 2] = temp_state[0, :, 2]
+        self.state[2, :, 0] = np.flip(temp_state[4, :, 2], 0)
+        self.state[5, :, 2] = np.flip(temp_state[2, :, 0], 0)
+        self.state[1, :] = np.rot90(temp_state[1, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def right_rev(self):
+        # Rotate the right face counter-clockwise
+        cd.right_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, :, 2] = temp_state[4, :, 2]
+        self.state[4, :, 2] = np.flip(temp_state[2, :, 0], 0)
+        self.state[2, :, 0] = np.flip(temp_state[5, :, 2], 0)
+        self.state[5, :, 2] = temp_state[0, :, 2]
+        self.state[1, :] = np.rot90(temp_state[1, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+    
+    def left(self):
+        # Rotate the left face clockwise
+        cd.left()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, :, 0] = temp_state[4, :, 0]
+        self.state[4, :, 0] = np.flip(temp_state[2, :, 2], 0)
+        self.state[2, :, 2] = np.flip(temp_state[5, :, 0], 0)
+        self.state[5, :, 0] = temp_state[0, :, 0]
+        self.state[3, :] = np.rot90(temp_state[3, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+              
+
+    def left_rev(self):
+        # Rotate the left face counter-clockwise
+        cd.left_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, :, 0] = temp_state[5, :, 0]
+        self.state[4, :, 0] = temp_state[0, :, 0]
+        self.state[2, :, 2] = np.flip(temp_state[4, :, 0], 0)
+        self.state[5, :, 0] = np.flip(temp_state[2, :, 2], 0)
+        self.state[3, :] = np.rot90(temp_state[3, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def bottom(self):
+        # Rotate the bottom face clockwise
+        cd.bottom()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, 2] = temp_state[3, 2]
+        self.state[3, 2] = temp_state[2, 2]
+        self.state[2, 2] = temp_state[1, 2]
+        self.state[1, 2] = temp_state[0, 2]
+        self.state[5, :] = np.rot90(temp_state[5, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def bottom_rev(self):
+        # Rotate the bottom face counter-clockwise
+        cd.bottom_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[0, 2] = temp_state[1, 2]
+        self.state[3, 2] = temp_state[0, 2]
+        self.state[2, 2] = temp_state[3, 2]
+        self.state[1, 2] = temp_state[2, 2]
+        self.state[5, :] = np.rot90(temp_state[5, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def back(self):
+        # Rotate the back face clockwise
+        cd.back()
+        temp_state = copy.deepcopy(self.state)
+        self.state[1, :, 2] = np.flip(temp_state[5, 2, :], 0)
+        self.state[5, 2, :] = temp_state[3, :, 0]
+        self.state[3, :, 0] = np.flip(temp_state[4, 0, :], 0)
+        self.state[4, 0, :] = temp_state[1, :, 2]
+        self.state[2, :] = np.rot90(temp_state[2, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def back_rev(self):
+        # Rotate the back face counter-clockwise
+        cd.back_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[1, :, 2] = temp_state[4, 0, :]
+        self.state[5, 2, :] = np.flip(temp_state[1, :, 2], 0)
+        self.state[3, :, 0] = temp_state[5, 2, :]
+        self.state[4, 0, :] = np.flip(temp_state[3, :, 0], 0)
+        self.state[2, :] = np.rot90(temp_state[2, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def front(self):
+        # Rotate the front face clockwise
+        cd.front()
+        temp_state = copy.deepcopy(self.state)
+        self.state[1, :, 0] = temp_state[4, 2, :]
+        self.state[4, 2, :] = np.flip(temp_state[3, :, 2], 0)
+        self.state[3, :, 2] = temp_state[5, 0, :]
+        self.state[5, 0, :] = np.flip(temp_state[1, :, 0], 0)
+        self.state[0, :] = np.rot90(temp_state[0, :], -1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+    def front_rev(self):
+        # Rotate the front face counter-clockwise
+        cd.front_rev()
+        temp_state = copy.deepcopy(self.state)
+        self.state[1, :, 0] = np.flip(temp_state[5, 0, :], 0)
+        self.state[4, 2, :] = temp_state[1, :, 0]
+        self.state[3, :, 2] = np.flip(temp_state[4, 2, :], 0)
+        self.state[5, 0, :] = temp_state[3, :, 2]
+        self.state[0, :] = np.rot90(temp_state[0, :], 1)
+        self.history.append(inspect.currentframe().f_code.co_name)
+        np.save('history.npy', self.history)
+
+if __name__ == '__main__':
+    cube = Cube()
+    cube.top()
+    print(cube.state)
 
