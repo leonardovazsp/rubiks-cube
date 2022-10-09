94a1f6332d0ebe9156b20bd9a27b2db882312f2a
Leonardo <leonardovazsp@gmail.com>
2022-10-09 18:58:34 +1000

    training file created

--- server/train.py
+++ server/train.py
@@ -1,16 +1,94 @@
-from datasets import load_dataset, Dataset
+import torch
+from model import RubiksCubeModel
+from make_dataset import SampleGenerator, Dataset
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
 
-## Load the dataset from disk. The dataset is stored in the `data` directory, split into train, test and val sets.
-## The inputs for the model are the images (jpg), and the labels are numpy files of tensor of shape (27, 6).
-## The labels are the one-hot encoded representation of the cube state (colors).
 
-dataset = load_dataset("image_folder", 
-                        data_files={"train": "data/train",
-                                    "test": "data/test", 
-                                    "val": "data/val"},
-                        split=["train", "test", "val"],
-                        label_type="numpy",
-                        image_ext=".jpg")
+class DeviceDataLoader():
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
 
-print(dataset)
+    def __iter__(self):
+        for b in self.dl: 
+            yield to_device(b, self.device)
 
+    def __len__(self):
+        return len(self.dl)
+
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+def get_model():
+    model = RubiksCubeModel()
+    to_device(model, device)
+    return model
+
+def get_data_loaders(train_ds, val_ds, batch_size):
+    train_loader = DeviceDataLoader(DataLoader(train_ds, batch_size, shuffle=True, num_workers=28, pin_memory=True), device)
+    val_loader = DeviceDataLoader(DataLoader(val_ds, batch_size, num_workers=28, pin_memory=True), device)
+    return train_loader, val_loader
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD, gamma=0.999):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    scheduler = ExponentialLR(optimizer, gamma=0.999)
+    for epoch in range(epochs):
+        train_results = []
+        # Training Phase 
+        for batch in train_loader:
+            result = model.training_step(batch)
+            loss = result['loss']
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            train_results.append(result)
+        scheduler.step()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack([x.get('loss') for x in train_results]).mean().item()
+        result['train_acc'] = torch.stack([x.get('acc') for x in train_results]).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+if __name__ == '__main__':
+    import numpy as np
+    import json
+
+    config = json.load(open('config.json'))
+    batch_size = config['train']['batch_size']
+    epochs = config['train']['epochs']
+    gamma = config['train']['learning_rate_decay']
+    lr = config['train']['learning_rate']
+    optimizer = torch.optim.SGD if 'optimizer' not in config or config['train']['optimizer'] == 'SGD' else torch.optim.Adam
+    input_shape = config['train']['input_shape']
+    resolution = input_shape[1:]
+    
+    device = get_default_device()
+    data_dir = config['data_dir']
+
+    train_ds = Dataset(resolution, data_dir, 'train')
+    val_ds = Dataset(resolution, data_dir, 'val')
+    train_loader, val_loader = get_data_loaders(train_ds, val_ds, batch_size)
+
+    model = get_model()
+
+    history = [evaluate(model, val_loader)]
+    history += fit(epochs, lr, model, train_loader, val_loader, opt_func=optimizer, gamma=gamma)
+    torch.save(model.state_dict(), 'models/model_val_acc_{:.4f}_train_acc_{:.4f}.pth'.format(history[-1]['val_acc'], history[-1]['train_acc']))
+    
