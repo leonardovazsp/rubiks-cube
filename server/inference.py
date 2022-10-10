4e2e7f38f7ce744fabaea18790a8cb482eabc763
Leonardo <leonardovazsp@gmail.com>
2022-10-10 21:04:36 +1000

    inference.py adjusted and working ok - still need to create class

--- server/inference.py
+++ server/inference.py
@@ -1,9 +1,17 @@
 from model import RubiksCubeModel
 from data_pipeline import SampleGenerator
 import json
+import torch
+import numpy as np
+from PIL import Image
+import copy
+import random
 
 config = json.load(open('config.json'))
-model = RubiksCubeModel.load_from_checkpoint(config['model_path'])
+resolution = config['train']['input_shape'][1:]
+model = RubiksCubeModel()
+model.load_state_dict(torch.load(config['model_path']))
+
 
 def process_preds(pred_1, pred_2):
     # Join two labels together (inverse of pre_process_label function)
@@ -17,30 +25,30 @@ def process_preds(pred_1, pred_2):
     return pred
 
 def preprocess_img(img):
-    image = Image.fromarray(image)
-    image = image.resize(self.resolution)
-    image = np.array(image)/255
-    light = np.linspace(0.5, 2.5, 10).reshape(-1, 1, 1, 1)
-    image = image * light
-    return image.transpose(0, 3, 1, 2)
+    img = Image.fromarray(img)
+    img = img.resize(resolution)
+    img = np.array(img)/255
+    light = np.linspace(1, 2.5, 10).reshape(-1, 1, 1, 1)
+    img = np.clip(img * light, 0, 1)
+    return img.transpose(0, 3, 1, 2)
 
 def predict(img_1, img_2):
-    pred_1 = model.predict(preprocess_img(img_1))
-    pred_2 = model.predict(preprocess_img(img_2))
+    imgs = np.vstack([preprocess_img(img_1), preprocess_img(img_2)])
+    imgs = torch.from_numpy(imgs).float()
+    pred = model(imgs)
+    pred_1 = pred[:10].mean(axis=0).argmax(-1, keepdims=False).view(3, 3, 3).numpy()
+    pred_2 = pred[10:].mean(axis=0).argmax(-1, keepdims=False).view(3, 3, 3).numpy()
     pred = process_preds(pred_1, pred_2)
+    print(pred)
     return pred
 
 def evaluate(img_1, img_2, label):
     pred = predict(img_1, img_2)
-    diff = (pred >= 0.5) != label
-    total_difference = diff.max()
-    total_error = diff.sum()
-    wrong_types = diff.sum((1, 2))
-    return {
-        'total_difference': total_difference,
-        'total_error': total_error,
-        'wrong_types': wrong_types
-    }
+    acc = (pred == label).sum() / 54
+    return acc
 
-img_1, img_2, labels = SampleGenerator().get_imgs()
+if __name__ == '__main__':
+    img_1, img_2, labels = SampleGenerator().get_imgs()
+    acc = evaluate(img_1, img_2, labels)
+    print(acc)
 
